f64c14a22d1db990189886cc74a4246d7b5009a3
carlosfernandocr@ufps.edu.co <carlosfernandocr@ufps.edu.co>
2021-07-13 18:39:37 -0500

    added admin-service

---
+++ admin-service/src/test/java/co/pragma/adminservice/AdminServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package co.pragma.adminservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
